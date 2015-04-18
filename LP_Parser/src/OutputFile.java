97a2d59fa0f08f62546cb5ce30e59e01048a0319
BillKalaitzis <it145@uom.edu.gr>
2015-04-18 12:12:01 +0300

    Initilize OutputFile class & add writeFile method

---
+++ LP_Parser/src/OutputFile.java
@@ -0,0 +1,50 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class OutputFile {
+
+	protected BufferedWriter bw;
+	
+	public OutputFile(ArrayList<ArrayList<Integer>> A, ArrayList<Integer> b, ArrayList<Integer> c,ArrayList<Integer> Eqin){
+		
+		super();
+		writeFile(A,b,c,Eqin);
+		System.out.println("Data successfully saved on file");
+		
+	}
+	
+	public void writeFile(ArrayList<ArrayList<Integer>> A, ArrayList<Integer> b, ArrayList<Integer> c,ArrayList<Integer> Eqin){
+		
+		File output = new File("Data/output.txt");
+		try {
+			bw =  new BufferedWriter(new FileWriter(output));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			bw.write("A = " );
+			int counter = 0;
+			for(ArrayList<Integer> i:A){
+				if(counter == 0)
+					bw.write(i.toString() + "\n");
+				else
+					bw.write("    " + i.toString()+ "\n");
+				counter++;
+			}
+			bw.write("\nb = " + b + "\n\n");
+			bw.write("c = " + c + "\n\n");
+			bw.write("Eqin = " + Eqin);
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+}
